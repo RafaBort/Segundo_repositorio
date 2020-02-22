6a8a58c87988ebc6142538cbbd094aea812a2270
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-02-21 22:25:51 -0300

    modifiquei o texto do console.writeline

---
+++ programacao2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programacao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aula repositório");
+        }
+    }
+}
